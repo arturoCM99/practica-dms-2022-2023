6a1ca3ad0ed4e7c71443831b359d6dfe83ab78e5
arturo1026 <acm1026@alu.ubu.es>
2022-11-10 15:00:49 +0100

    Actualizacion

--- components/dms2223frontend/dms2223frontend/presentation/web/discussionendpoints.py
+++ components/dms2223frontend/dms2223frontend/presentation/web/discussionendpoints.py
@@ -179,3 +179,39 @@ class DiscussionEndpoints():
         redirect_to = request.args.get('redirect_to', default='/discussion/discussions')
         return redirect(redirect_to)
 
+    def get_discussion_discussions_report(auth_service: AuthService) -> Union[Response, Text]:
+        """ Handles the GET requests to the discussion root endpoint.
+
+        Args:
+            - auth_service (AuthService): The authentication service.
+
+        Returns:
+            - Union[Response,Text]: The generated response to the request.
+        """
+        if not WebAuth.test_token(auth_service):
+            return redirect(url_for('get_login'))
+        if Role.DISCUSSION.name not in session['roles']:
+            return redirect(url_for('get_home'))
+        name = session['user']
+        title: str = str(request.args.get('discussiontitle'))
+        redirect_to = request.args.get('redirect_to', default='/discussion/discussions/view')
+        return render_template('discussion/discussions/report.html', name=name, roles=session['roles'], redirect_to=redirect_to, title=title)
+
+
+    def post_discussion_discussions_report(auth_service: AuthService) -> Union[Response, Text]:
+        """ Handles the POST requests to the discussion root endpoint.
+
+        Args:
+            - auth_service (AuthService): The authentication service.
+
+        Returns:
+            - Union[Response,Text]: The generated response to the request.
+        """
+        if not WebAuth.test_token(auth_service):
+            return redirect(url_for('get_login'))
+        if Role.DISCUSSION.name not in session['roles']:
+            return redirect(url_for('get_home'))
+
+        redirect_to = request.args.get('redirect_to', default='/discussion/discussions')
+        return redirect(redirect_to)
+
